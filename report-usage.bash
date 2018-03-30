282c1e09cff119b8e69d072b6e44c298e8fee7ba
dfwmtc <36609953+dfwmtc@users.noreply.github.com>
2018-03-30 13:51:25 -0500

    Add files via upload

---
+++ report-usage.bash
@@ -0,0 +1,8 @@
+df -PhT | awk '{print $3,$4,$5,$7}'
+echo "*****************************"
+top -b -n1 | grep "Cpu(s)" | awk '{print $2 + $4,$5}'
+echo "*************************"
+egrep 'processor|core id' /proc/cpuinfo
+echo "***************"
+free -m
+echo "****************************"
